9759af5a0d060d4ffe1fe317f501541a3d080abf
Alex <mpungu2014@gmail.com>
2023-07-03 13:17:16 -0400

    terraform

---
+++ Variable.tf
@@ -0,0 +1,25 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+
+}
+variable "profile" {
+  default = "default"
+
+}
+variable "topic_name" {
+  default = "sns_Dev"
+
+}
+variable "team" {
+  default = "Devops"
+
+}
+variable "endpoint" {
+  default = "mpungu2014@gmail.com"
+
+}
+variable "protocol" {
+  default = "email"
+
+}
---
+++ main.tf
@@ -0,0 +1,13 @@
+
+
+resource "aws_sns_topic" "demo" {
+  name = var.topic_name
+  tags = {
+    team = var.team
+  }
+}
+resource "aws_sns_topic_subscription" "email-target" {
+  topic_arn = aws_sns_topic.demo.arn
+  protocol  = var.protocol
+  endpoint  = var.endpoint
+}
